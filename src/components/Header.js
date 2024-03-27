084852681cb3c938bfc303bd0af756832425131a
brynjarmbje <129754325+brynjarmbje@users.noreply.github.com>
2024-03-27 00:22:40 +0000

    header upd and css for nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { FaGithub, FaHome, FaMusic, FaSoundcloud, FaUser } from 'react-icons/fa';
 
 function Header() {
     return (
         <header className="App-header">
             <nav>
                 <ul>
-                    <li><a href="#music">Music</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="/"><FaHome /></a></li>
+                    <li><a href="/music"><FaMusic /></a></li>
+                    <li><a href="/bio"><FaUser /></a></li>
                 </ul>
             </nav>
-            <h1>Welcome to My Music App</h1>
+            <div>
+                <a href="https://github.com/brynjarmbje"><FaGithub /></a>
+                <a href="https://soundcloud.com/brynjar-mar-bjornsson"><FaSoundcloud /></a>
+            </div>
         </header>
     );
 }
