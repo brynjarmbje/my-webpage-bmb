5cae5486a1116ec6e4672a528dffaad5841e36bd
brynjarmbje <129754325+brynjarmbje@users.noreply.github.com>
2024-03-31 21:29:09 +0000

    music page  upd

--- src/components/FeaturedProject.js
+++ src/components/FeaturedProject.js
@@ -2,9 +2,8 @@ import React from 'react';
 
 const FeaturedProject = () => (
   <div className="featured-project">
-    <h3>Featured Project</h3>
-    <p>A highlight of my latest work in music composition for visual media.</p>
-    {/* Include a media player or a link to the project */}
+    <h3>Trailer <a href='/music'>music</a></h3>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/mlUvBIheVMc?si=6dS18SlLmSJniPV0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
   </div>
 );
 export default FeaturedProject;
--- src/components/MusicPage.js
+++ src/components/MusicPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import '../styles/MusicPage.css';
 
 const songs = [
@@ -14,21 +14,56 @@ const songs = [
         url: "https://storage.cloud.google.com/music-files-mycreations-bmb/SongsforSubmit/BolemDastnnu.wav",
         type: "audio/wav",
     },
-  // Add more songs as needed
+    // Add more songs as needed
 ];
 
 const MusicPage = () => {
-  return (
-    <div className="music-page">
-      {songs.map((song, index) => (
-        <div className="music-player-container" key={index}>
-          <h2 className="song-title">{song.title}</h2>
-          <h4>{song.author}</h4>
-          <audio className="audio-player" controls src={song.url}></audio>
+    const songRefs = useRef([]);
+
+    useEffect(() => {
+        songRefs.current = songRefs.current.slice(0, songs.length);
+
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        entry.target.play().catch(err => console.error("Error trying to play the song:", err));
+                    } else {
+                        entry.target.pause();
+                    }
+                });
+            },
+            { threshold: 0.5 } // This threshold triggers the callback when 50% of the item is visible
+        );
+
+        songRefs.current.forEach(ref => {
+            if (ref) observer.observe(ref);
+        });
+
+        return () => {
+            songRefs.current.forEach(ref => {
+                if (ref) observer.unobserve(ref);
+            });
+        };
+    }, []);
+
+    return (
+        <div className="music-page">
+            {songs.map((song, index) => (
+                <section className="music-player-container" key={index}>
+                    <h2 className="song-title">{song.title}</h2>
+                    <h4>{song.author}</h4>
+                    <audio
+                        ref={el => songRefs.current[index] = el}
+                        className="audio-player"
+                        controls
+                        src={song.url}
+                        preload="none"
+                    ></audio>
+                </section>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default MusicPage;
