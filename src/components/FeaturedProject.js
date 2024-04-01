93ab269f4f9dccb977f58d4431f6e91ecfc269c8
brynjarmbje <129754325+brynjarmbje@users.noreply.github.com>
2024-04-01 21:35:03 +0000

    iframe fix

--- src/components/FeaturedProject.js
+++ src/components/FeaturedProject.js
@@ -3,7 +3,7 @@ import React from 'react';
 const FeaturedProject = () => (
   <div className="featured-project">
     <h3>Trailer <a href='/music'>music</a></h3>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/mlUvBIheVMc?si=6dS18SlLmSJniPV0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+    <iframe  className='responsive-iframe' width="560" height="315" src="https://www.youtube.com/embed/mlUvBIheVMc?si=6dS18SlLmSJniPV0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
   </div>
 );
 export default FeaturedProject;
