1d5b3c1ae1ab648ebd424c8fa35ed3f3a3842e5a
brynjarmbje <129754325+brynjarmbje@users.noreply.github.com>
2024-04-24 14:38:34 +0000

    ehv

--- src/components/AboutTeaser.js
+++ src/components/AboutTeaser.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 const AboutTeaser = () => (
   <div className="about-teaser">
-    <h3>    <Link to="/bio">About Me</Link></h3>
-    <p>A multifaceted professional with a rich background in musical composition and an emerging career in computer science and web development.</p>
+    <h3>    <Link to="/bio">Visit my bio page</Link></h3>
+    <p>"Blending a vibrant background in musical composition with robust web development skills, I navigate seamlessly between the creative realms of music and the logical demands of computer science. With years of experience from the stages of Whisky a Go Go to coding at the cutting edge with Fly PLAY, I thrive in dynamic environments that challenge both my artistic and technical prowess."</p>
   </div>
 );
 export default AboutTeaser;
