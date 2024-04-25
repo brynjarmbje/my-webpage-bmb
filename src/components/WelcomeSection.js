b56cd4d5526d7e205bdb35be700be8d52ef325f4
brynjarmbje <129754325+brynjarmbje@users.noreply.github.com>
2024-04-25 14:27:08 +0000

    font and logo

--- src/components/WelcomeSection.js
+++ src/components/WelcomeSection.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const WelcomeSection = () => (
   <div className="welcome-section">
-    <h2>Brynjar Már Björnsson is a programmer, developer and music composer.</h2>
+    <h2>Programmer, developer and music composer.</h2>
     <p>Creating meaningful and useful solutions.</p>
   </div>
 );
